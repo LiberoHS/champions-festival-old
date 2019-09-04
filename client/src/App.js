0381ee6ab478f9cb34a32549dda8388b08e3814f
Jeremy Lim <jeremy.lim1636@gmail.com>
2019-09-05 03:00:11 +1000

    v1.3 set up server

--- client/src/App.js
+++ client/src/App.js
@@ -102,7 +102,8 @@ class App extends React.Component {
         ],
         currentTournament: null,
         showTournament: false,
-        showList: false
+        showList: false,
+        data: null
     }
 
     setCurrentTournament = (tournament) => {
@@ -120,6 +121,24 @@ class App extends React.Component {
     tournamentInfo = () => {
         this.setState({ showTournament: true, showList: false });
     }
+
+    // EXPRESS BACK-END
+    componentDidMount() {
+        // Call our fetch function below once the component mounts
+        this.callBackendAPI()
+        .then(res => this.setState({ data: res.express }))
+        .catch(err => console.log(err));
+    }
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+    callBackendAPI = async () => {
+        const response = await fetch('/express_backend');
+        const body = await response.json();
+
+        if (response.status !== 200) {
+            throw Error(body.message)
+        }
+        return body;
+    };
 
     render () {
         const { tournamentList, currentTournament, decks} = this.state;
